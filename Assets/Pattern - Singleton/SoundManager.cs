a425b296ec6359e6523b408847b4bbf8bb844348
Cristian Cruz <cristiankhalil@gmail.com>
2017-02-08 20:53:55 +0100

    Pattern - Singleton

---
+++ Assets/Pattern - Singleton/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            DestroyObject(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOurVerySpecialSound()
+    {
+        audioSource.Play();
+    }
+
+}
---
+++ Assets/Pattern - Singleton/UIButtonsFunctions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIButtonsFunctions : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text soundManagerInstanceIDTxt;
+
+    // Use this for initialization
+    void Start()
+    {
+        soundManagerInstanceIDTxt.text = $"SoundManager instance ID: {SoundManager.instance.GetInstanceID().ToString()}";
+    }
+
+    public void CallTheSoundManagerAndPlayTheSpecialSound()
+    {
+        SoundManager.instance.PlayOurVerySpecialSound();
+    }
+
+    public void ChangeScene()
+    {
+        if (SceneManager.GetActiveScene().name == "MainSingleton1")
+            SceneManager.LoadScene("MainSingleton2", LoadSceneMode.Single);
+        else
+            SceneManager.LoadScene("MainSingleton1", LoadSceneMode.Single);
+    }
+
+}
